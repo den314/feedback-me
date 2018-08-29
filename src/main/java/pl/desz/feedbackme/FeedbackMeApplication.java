3af4ca2dea2e2781d091e60b480579c8a19e3469
Denis <dev.szczukocki.denis@gmail.com>
2018-08-29 22:17:08 +0200

    init

---
+++ src/main/java/pl/desz/feedbackme/FeedbackMeApplication.java
@@ -0,0 +1,12 @@
+package pl.desz.feedbackme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedbackMeApplication.class, args);
+    }
+}
